858fa9d0c05034cc38859f0ee9240bf16f42d535
Nohely Badillo <nohely.badillo96@gmail.com>
2022-12-29 10:22:37 -0600

    create a function to flat arrays

---
+++ main.py
@@ -0,0 +1,13 @@
+import logging
+
+def flat_lists(lists: list) -> list:
+    try:
+        flat_list = []
+        for item in lists:
+            if type(item) == list:
+                flat_list = flat_list + flat_lists(item)
+            else:
+                flat_list.append(item)
+        return flat_list
+    except Exception as e:
+        logging.error(f' An error has occurred {e}')
